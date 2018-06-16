8e97b0401b12cc61856052f55aecbe9d69cad74e
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-16 19:23:01 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
     i = 0
     array = []
     while i < collection.length
-      if yield collection[i] == true
+      if yield (collection[i]) == true
         array.push(collection[i])
       end
     end
